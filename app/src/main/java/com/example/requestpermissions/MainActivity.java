d019215aa11e403a4069be5e2a98e2cc31535dac
Péricles Gomes <prgo.development@gmail.com>
2022-09-18 11:27:47 -0300

    Preparing

--- app/src/main/java/com/example/requestpermissions/MainActivity.java
+++ app/src/main/java/com/example/requestpermissions/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.requestpermissions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private static final int STORAGE_PERMISSION_CODE = 100;
+    private static final String TAG = "PERMISSION_TAG";
+
+    private EditText etxt_nameNewFolder;
+    private Button btn_save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etxt_nameNewFolder = findViewById(R.id.etxt_nameNewFolder);
+        btn_save = findViewById(R.id.btn_save);
     }
 }
